4962a1af41d10d064ed34f1a1c858471b4e8ceff
Renato Silva <resilva87@outlook.com>
2017-04-03 23:10:14 -0300

    Add arbitrary instance to check MyList with checkers

--- Applicatives.hs
+++ Applicatives.hs
@@ -22,3 +22,27 @@ instance Applicative MyList where
     (<*>) Nil _   = Nil
     (<*>) _   Nil = Nil
     (<*>) (Cons f g) (Cons x y) = Cons (f x) (Cons f Nil <*> y) `mappend` (g <*> Cons x y)
+
+instance (Arbitrary a) => Arbitrary (MyList a) where
+     arbitrary = (Cons <$> arbitrary <*> oneof [arbitrary, return Nil])
+
+instance (Eq a) => EqProp (MyList a) where (=-=) = eq
+
+append :: MyList a -> MyList a -> MyList a
+append Nil ys = ys
+append (Cons x xs) ys = Cons x $ xs `append` ys
+
+fold :: (a -> b -> b) -> b -> MyList a -> b
+fold _ b Nil = b
+fold f b (Cons h t) = f h (fold f b t)
+
+concat' :: MyList (MyList a) -> MyList a
+concat' = fold append Nil
+
+flatMap :: (a -> MyList b) -> MyList a -> MyList b
+flatMap f as = concat' $ fmap f as
+
+main :: IO ()
+main = do
+    quickBatch $ monoid (Cons (1::Int) Nil)
+    quickBatch $ applicative $ (undefined :: MyList (String, Int, Double))
