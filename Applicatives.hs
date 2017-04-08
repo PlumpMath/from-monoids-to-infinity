47e1aecd77a222d5fd414fc24010714ce20b062e
Renato Silva <resilva87@outlook.com>
2017-04-08 18:10:24 -0300

    Monad MySum, add some references and commentary

--- Applicatives.hs
+++ Applicatives.hs
@@ -1,11 +1,14 @@
 module Applicatives where
 
 import Control.Applicative
+import Control.Monad (liftM)
 import Data.Monoid
 import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
 
+-------------------------- MyList -------------------------
+-----------------------------------------------------------
 data MyList a = Nil | Cons a (MyList a) deriving (Eq, Show)
 
 instance Monoid (MyList a) where
@@ -55,6 +58,8 @@ zipWith' :: (a-> b -> c) -> MyList a -> MyList b -> MyList c
 zipWith' f (Cons x x') (Cons y y') = Cons (f x y) $ zipWith' f x' y'
 zipWith' _ _      _      = Nil
 
+------------------------ MyZipList ------------------------
+-----------------------------------------------------------
 newtype MyZipList a = MyZipList (MyList a) deriving (Eq, Show)
 
 instance Eq a => EqProp (MyZipList a) where
@@ -74,8 +79,37 @@ instance Applicative MyZipList where
 instance (Arbitrary a) => Arbitrary (MyZipList a) where
     arbitrary = MyZipList <$> arbitrary
 
+--------------------------- MySum -------------------------
+-----------------------------------------------------------
+data MySum a b = MyFirst a | MySecond b deriving (Eq, Show)
+
+instance Functor (MySum a) where
+    fmap _ (MyFirst x) = MyFirst x
+    fmap f (MySecond x) = MySecond (f x)
+
+instance Applicative (MySum a) where
+    pure = MySecond
+    (MyFirst a) <*> _ = MyFirst a
+    (MySecond f) <*> v = f <$> v
+
+instance Monad (MySum a) where
+    return = pure
+    (MyFirst x) >>= _ = MyFirst x
+    (MySecond x) >>= k = k x
+
+instance (EqProp a, EqProp b) => EqProp (MySum a b) where
+    (MyFirst x) =-= (MyFirst y) = x =-= y
+    (MySecond x) =-= (MySecond y) = x =-= y
+    _            =-= _            = property False
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (MySum a b) where
+     arbitrary = oneof [liftM MyFirst arbitrary, liftM MySecond arbitrary]
+     shrink (MyFirst x)  = [MyFirst  x' | x' <- shrink x ]
+     shrink (MySecond y) = [MySecond y' | y' <- shrink y]
+
 main :: IO ()
 main = do
-    quickBatch $ monoid $ (undefined :: MyList Int)
     quickBatch $ applicative $ (undefined :: MyList (String, Int, Double))
     quickBatch $ applicative $ (undefined :: MyZipList (String, Int, Double))
+    quickBatch $ applicative $ (undefined :: MySum String (Int, Double, Char))
+    quickBatch $ monad $ (undefined :: MySum String (Int, Double, Char))
